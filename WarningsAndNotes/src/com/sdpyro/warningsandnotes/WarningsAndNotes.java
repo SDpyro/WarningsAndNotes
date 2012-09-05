42fa254bf3abff6f0f98c3f993aa5ccefb3e8c88
Fernando Freire <dogonthehorizon@gmail.com>
2012-09-05 13:20:56 -0700

    Added simple logging functions.

--- WarningsAndNotes/src/com/sdpyro/warningsandnotes/WarningsAndNotes.java
+++ WarningsAndNotes/src/com/sdpyro/warningsandnotes/WarningsAndNotes.java
@@ -3,8 +3,16 @@ package com.sdpyro.warningsandnotes;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class WarningsAndNotes extends JavaPlugin {
-	public void main(){
-		System.out.println("Hello World.");
+	@Override
+	public void onEnable(){
+		//TODO Insert logic to be performed when plugin is enabled.
+		getLogger().info("WarningsAndNotes onEnable invoked!");
+	}
+	
+	@Override
+	public void onDisable(){
+		//TODO Insert loic to be performed when plugin is disabled.
+		getLogger().info("WarningsAndNotes on Disabled invoked!");
 	}
 
 }
