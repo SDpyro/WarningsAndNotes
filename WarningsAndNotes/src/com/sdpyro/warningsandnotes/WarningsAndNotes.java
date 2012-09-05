99dfa65b141da154afd108fb34d9115c6099c0be
Fernando Freire <dogonthehorizon@gmail.com>
2012-09-05 13:12:51 -0700

    Reconfiguring packages and classes to conform to bukkit standards.

---
+++ WarningsAndNotes/src/com/sdpyro/warningsandnotes/WarningsAndNotes.java
@@ -0,0 +1,10 @@
+package com.sdpyro.warningsandnotes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WarningsAndNotes extends JavaPlugin {
+	public void main(){
+		System.out.println("Hello World.");
+	}
+
+}
